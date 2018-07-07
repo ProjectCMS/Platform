47b3cf68fa0cd982da82ddb40659e7ba2b7ee97e
Michel Vieira <michelvieira@outlook.com>
2018-07-07 20:49:24 -0300

    Update

--- Modules/Users/Http/routes.php
+++ Modules/Users/Http/routes.php
@@ -5,6 +5,7 @@
         'prefix'     => 'admin',
         'namespace'  => 'Modules\Users\Http\Controllers'
     ], function() {
+        Route::get('/', 'LoginController@showLoginForm')->name('login');
         Route::get('/login', 'LoginController@showLoginForm')->name('login');
         Route::post('/login', 'LoginController@login');
         Route::post('/logout', 'LoginController@logout')->name('logout');
