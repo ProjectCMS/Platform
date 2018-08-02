7930f87d20f3b60d38dab84e9ee0fe441727cfaa
Michel <michelvieira@outlook.com>
2018-08-02 14:00:55 -0300

    Update

--- Modules/Users/Http/Controllers/Auth/ForgotPasswordController.php
+++ Modules/Users/Http/Controllers/Auth/ForgotPasswordController.php
@@ -1,6 +1,6 @@
 <?php
 
-    namespace Modules\Users\Http\Controllers;
+    namespace Modules\Users\Http\Controllers\Auth;
 
     use App\Http\Controllers\Controller;
     use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
