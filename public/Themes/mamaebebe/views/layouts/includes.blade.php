7be5862a0ad8df0ce17f2cb6f8f2deaf34fc5fe7
Michel Vieira <michelvieira@outlook.com>
2018-08-13 09:35:08 -0300

    Update

--- public/Themes/mamaebebe/views/layouts/includes.blade.php
+++ public/Themes/mamaebebe/views/layouts/includes.blade.php
@@ -41,6 +41,17 @@
     }(document, 'script', 'facebook-jssdk'));
 </script>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-82823579-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-82823579-1');
+</script>
+
+
 <script type="text/javascript">
 
     var web = {};
