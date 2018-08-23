fbcf349bc04c778ed1dcd3e0df8092e8814cd1d0
Michel Vieira <michelvieira@outlook.com>
2018-08-23 12:01:44 -0300

    Update

--- public/Themes/mamaebebe/webpack.mix.js
+++ public/Themes/mamaebebe/webpack.mix.js
@@ -13,10 +13,4 @@ mix.sass('resources/scss/theme.scss', 'assets/css').options(
  */
 mix.scripts(['../../Modules/**/web/compile/js/*.js'], 'assets/js/modules.min.js');
 mix.styles(['../../Modules/**/web/compile/css/*.css'], 'assets/css/modules.min.css');
-mix.styles([
-               'assets/css/libs.min.css',
-               'assets/css/core.min.css',
-               'assets/css/modules.min.css',
-               'assets/css/theme.css'
-           ], 'assets/css/all.min.css');
 
