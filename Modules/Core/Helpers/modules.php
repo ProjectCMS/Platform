cf2b5ca8200d18e8807a690cdefed6f5afbdb358
Michel Vieira <michelvieira@outlook.com>
2018-08-24 16:15:02 -0300

    Update

--- Modules/Core/Helpers/modules.php
+++ Modules/Core/Helpers/modules.php
@@ -76,7 +76,7 @@
     //** Image **//
     if (!function_exists('image_resize')) {
 
-        function image_resize ($url, $quality = 60, $w = null, $h = 300, $options = [])
+        function image_resize ($url, $quality = 60, $w = null, $h = 400, $options = [])
         {
             $url = public_path('storage/' . $url);
             $img = \Image::make($url)->resize($w, $h, function($constraint) {
