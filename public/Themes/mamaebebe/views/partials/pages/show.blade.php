72bbbf9ffa57e56a99a373333c375fcaa0cb682d
Michel Vieira <michelvieira@outlook.com>
2018-08-30 15:11:28 -0300

    Update

--- public/Themes/mamaebebe/views/partials/pages/show.blade.php
+++ public/Themes/mamaebebe/views/partials/pages/show.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.page')
 @section('page_title', $seo->title)
-@section('page_breadcrumb')
-    {{ Breadcrumbs::render('page', $page) }}
-@stop
 @section('page_content')
-    <div class="page-text">
-        {!! $page->content !!}
-    </div>
+    @if($page->content)
+        <div class="page-text">
+            {!! $page->content !!}
+        </div>
+    @endif
     @if($page->template)
         @include($page->template->path)
     @endif
